002ed32d279a2d844df67cf7bbcb683751f36891
Manish4Star <sf.manish4@gmail.com>
2018-09-18 16:08:18 -0400

    added functions of test cases

--- src/test/java/core/ArithmiticSolverTest.java
+++ src/test/java/core/ArithmiticSolverTest.java
@@ -6,27 +6,27 @@ public class ArithmiticSolverTest extends TestCase {
 	
 	public void testAdd() {
 		ArithmiticSolver solver = new ArithmiticSolver();
-		assertEquals(4, solver.add(2,2));
+		assertEquals(4.0, solver.add(2,2));
 	}
 	
 	public void testSubtract() {
 		ArithmiticSolver solver = new ArithmiticSolver();
-		assertEquals(2, solver.subtract(4,2));
+		assertEquals(2.0, solver.subtract(4,2));
 	}
 	
 	public void testMultiply() {
 		ArithmiticSolver solver = new ArithmiticSolver();
-		assertEquals(16, solver.multiply(8,2));
+		assertEquals(16.0, solver.multiply(8,2));
 	}
 	
 	public void testDivide() {
 		ArithmiticSolver solver = new ArithmiticSolver();
-		assertEquals(3, solver.divide(9,3));
+		assertEquals(3.0, solver.divide(9,3));
 	}
 	
 	public void testModulus() {
 		ArithmiticSolver solver = new ArithmiticSolver();
-		assertEquals(1, solver.modulus(5,2));
+		assertEquals(1.0, solver.modulus(5,2));
 	}
 
 }
